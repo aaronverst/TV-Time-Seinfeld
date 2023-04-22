b686aa1491394a8b8b4a9c15039c36fe01fbe610
Gengler22 <78129491+Gengler22@users.noreply.github.com>
2023-04-21 20:29:11 -0400

    Update CharacterData.js

--- js/CharacterData.js
+++ js/CharacterData.js
@@ -6,7 +6,7 @@ d3.csv('../data/scripts_updated.csv')
             // count += 1;
         })
 
-        
+
 
         const actorContainer = document.getElementById("character_dropdown");
 
@@ -14,36 +14,24 @@ d3.csv('../data/scripts_updated.csv')
             $(".character_dropdown-content").select2();
         });
 
-        let character = d3.rollups(data, v => v.length, d => d.Character, d => d.SEID);
-        
+        let aggregatedData = d3.rollups(data, v => v.length, d => d.Character, d => d.SEID);
+        dataMap = aggregatedData;
 
-        const aggregatedDataMap = d3.rollups(data, v => v.length, d => d.Character, d => d.SEID);
-        dataMap = aggregatedDataMap;
+        console.log(aggregatedData);
 
-        jerryData = dataMap[0];
-        georgeData = dataMap[1];
-        elaineData = dataMap[2];
-        kramerData = dataMap[3];
-        newmanData = dataMap[4];
-        mortyData = dataMap[5];
-        helenData = dataMap[6];
-        frankData = dataMap[7];
-        
-
-        console.log(aggregatedDataMap);
-
-        
-
-        // Filter the aggregatedData array to only include keys in the orderedKeys array
-        
-
-       for( let i = 0; i < aggregatedDataMap.length; i++) {
+        for (let i = 0; i < aggregatedData.length; i++) {
             var actorElement = document.createElement('option');
-            actorElement.value = aggregatedDataMap[i][0];
-            actorElement.innerHTML = aggregatedDataMap[i][0];
-        
+            actorElement.value = aggregatedData[i][0];
+            actorElement.innerHTML = aggregatedData[i][0];
+
             actorContainer.appendChild(actorElement);
         }
+
+        const orderedKeys = ['JERRY', 'GEORGE', 'ELAINE', 'KRAMER', 'NEWMAN', 'MORTY', 'HELEN', 'FRANK'];
+
+
+        let aggregatedDataMap = Array.from(aggregatedData, ([key, count]) => ({ key, count }));
+        aggregatedDataMap = aggregatedDataMap.filter(d => orderedKeys.includes(d.key));
 
     })
 
@@ -59,22 +47,58 @@ $("select.character_dropdown-content").change(updateActor);
 
 function updateActor() {
     var numEpisodes = $('select.character_dropdown-content').val();
+    let aggregatedData = d3.rollups(_data, v => v.length, d => d.Character, d => d.SEID);
+    const orderedKeys = ['JERRY', 'GEORGE', 'ELAINE', 'KRAMER', 'NEWMAN', 'MORTY', 'HELEN', 'FRANK'];
+
+    let aggregatedDataMap = Array.from(aggregatedData, ([key, count]) => ({ key, count }));
+    aggregatedDataMap = aggregatedDataMap.filter(d => orderedKeys.includes(d.key));
+
+
 
     $('.c-container')
         .find('.character-container')
         .hide()
         .filter(function () {
-            var okEpisode = false;  
+            var okEpisode = false;
 
-            if (numEpisodes !== "None") {
+            if (numEpisodes != "None") {
                 okEpisode = $(this).attr('data-character') === numEpisodes;
                 console.log($(this).attr('data-character'));
                 console.log(okEpisode);
                 console.log(numEpisodes);
-                if (okEpisode == true) {
-                    return okEpisode; 
-                    }
+                if ($(this).attr('data-character') == numEpisodes && numEpisodes == "JERRY" && $(this).attr('data-character') == "JERRY" && okEpisode == true) {
+                    linesPerEpisode = new LinesPerEpisode({ parentElement: '#linesPerEpisodeJerry' }, jerryData);
+                    linesPerEpisode.updateVis();
+                    return okEpisode;
+                }
+                else if ($(this).attr('data-character') == numEpisodes && numEpisodes == "GEORGE" && $(this).attr('data-character') == "GEORGE" && okEpisode == true) {
+                    linesPerEpisode = new LinesPerEpisode({ parentElement: '#linesPerEpisodeGeorge' }, georgeData);
+                    linesPerEpisode.updateVis();
+                    return okEpisode;
+                }
+                else if ($(this).attr('data-character') == numEpisodes && numEpisodes == "ELAINE" && $(this).attr('data-character') == "ELAINE" && okEpisode == true) {
+                    linesPerEpisode = new LinesPerEpisode({ parentElement: '#linesPerEpisodeElaine' }, elaineData);
+                    linesPerEpisode.updateVis();
+                    return okEpisode;
+                }
+                else if ($(this).attr('data-character') == numEpisodes && numEpisodes == "KRAMER" && $(this).attr('data-character') == "KRAMER" && okEpisode == true) {
+                    linesPerEpisode = new LinesPerEpisode({ parentElement: '#linesPerEpisodeKramer' }, kramerData);
+                    linesPerEpisode.updateVis();
+                    return okEpisode;
+                }
+                else if ($(this).attr('data-character') == numEpisodes && numEpisodes == "NEWMAN" && $(this).attr('data-character') == "NEWMAN" && okEpisode == true) {
+                    linesPerEpisode = new LinesPerEpisode({ parentElement: '#linesPerEpisodeNewman' }, newmanData);
+                    linesPerEpisode.updateVis();
+                    return okEpisode;
+                }
+                else if ($(this).attr('data-character') == numEpisodes && numEpisodes == "MORTY" && $(this).attr('data-character') == "MORTY" && okEpisode == true) {
+                    linesPerEpisode = new LinesPerEpisode({ parentElement: '#linesPerEpisodeMorty' }, mortyData);
+                    linesPerEpisode.updateVis();
+                    return okEpisode;
+                }
             }
+
+
 
         }).fadeIn('fast');
 
